e86b96827c6b59139fafcae18a89bb825d220e1e
Himanshu Gupta <121546558+Crazeecoder01@users.noreply.github.com>
2026-01-05 12:03:20 +0530

    Added book repository

---
+++ BookRepository.java
@@ -0,0 +1,26 @@
+package com.bookstore.booklibrary.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.bookstore.booklibrary.model.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer>{
+    @Query("""
+        SELECT b FROM Book b
+        WHERE LOWER(b.title) LIKE LOWER(CONCAT('%', :keyword, '%'))
+           OR LOWER(b.author) LIKE LOWER(CONCAT('%', :keyword, '%'))
+           OR LOWER(b.isbn) LIKE LOWER(CONCAT('%', :keyword, '%'))
+    """)
+    List<Book> searchBooks(String keyword);
+
+    @Query("""
+        SELECT b FROM Book b
+        WHERE  LOWER(b.author) LIKE LOWER(CONCAT('%', :keyword, '%'))
+    """)
+    List<Book> searchBooksByAuthor(String keyword);
+}
